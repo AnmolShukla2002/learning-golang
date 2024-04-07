a99f8574bc284716b0b4477a9485b79c013383ca
Anmol Shukla <anmol.28422@gmail.com>
2024-04-08 02:12:02 +0530

    printf and println

---
+++ print/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "Anmol"
+	height := 6.098
+
+	fmt.Println("name:",name, " age:",age, " height:",height)
+	fmt.Println("-------------------------------------------")
+	fmt.Printf("Age is %d\n",age)
+}
